2eafa5073ec97d2d2a344fe6c9b28d749f353685
Kopycisko <rafszt@wp.pl>
2015-11-15 16:25:32 +0100

    kolejne proby

--- mobilne/objdbver/envtests/sqlalch_kivy.py
+++ mobilne/objdbver/envtests/sqlalch_kivy.py
@@ -1,4 +1,3 @@
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import sessionmaker
@@ -17,8 +16,11 @@ DATABASE = 'sqlite:///db.sqlite3'
 DEBUG = True
 
 # ORM base
-Base = declarative_base()
+import base
 
+# ORM types
+# import myrec
+from myrec import MyRecord
 
 # Connect to database
 engine = create_engine(DATABASE, echo=DEBUG)
@@ -26,26 +28,10 @@ session_factory = sessionmaker(bind=engine)
 session = session_factory()
 
 # Initialize database if it doesn't exist
-if not os.path.exists('db.sqlite3'):
-  Base.metadata.create_all(engine)
+#if not os.path.exists('db.sqlite3'):
+base.Base.metadata.create_all(engine)
 
-# ORM types
-class MyRecord(Base):
-    __tablename__ = 'records'
-
-    id = Column(Integer, primary_key=True)
-    key = Column(String, nullable=False)
-    value = Column(String, nullable=False)
-
-    def __init__(self, key, value):
-      self.key = key
-      self.value = value
-
-    def __repr__(self):
-        return "<Record('%s = %s')>" % (self.key, self.value)
-
-
-# Create a bunch of records if we don't have any
+# Create a bunch of records if we dozn't have any
 if session.query(MyRecord).count() == 0:
   for i in range(10):
     record = MyRecord('key%d' % i, 'value%d' % i)
--- mobilne/objdbver/models/dbmgr.py
+++ mobilne/objdbver/models/dbmgr.py
@@ -7,76 +7,65 @@ from sqlalchemy.orm import relationship, backref
 import os.path
 #ORM types
 from datetime import datetime
-# from listmodel import ListModel
+import base
+from listmodel import ListModel
+
+# Database connection
+DB_FILE = 'pom_db.sqlite'
+DB_LINK = 'sqlite:///' + DB_FILE
+DEBUG = True
+
+""" Connect to database
+"""
+engine = create_engine(DB_LINK, echo=DEBUG)
+session_factory = sessionmaker(bind=engine)
+session = session_factory()
+
+""" Init database if it doesn't exists
+"""
+
+base.Base.metadata.create_all(engine)
+session.query(ListModel).count()
+lst = ListModel("name", 1)
+session.add(lst)
+session.commit()
+
+	#   Base.metadata.create_all(self.engine)
+	# # Create a bunch of records if we don't have any
+	# if self.session.query(ListModel).count() == 0:
+	# 	print("Creating new list in database")
+	# 	lst = ListModel(name, user_id)
+	# 	self.session.add(lst)
 
 
-Base = declarative_base()
+def ChangeList(self, list_id, name):
+	""" Perform changes in given list in database
+		list_id - id of the list
+		name - name of the list
+		user_id - id of the owner
+	returns: new List
+	"""
+	print("Modyfing list data in database")
+	lst = GetSingleList(list_id)
+	lst.Modify(name)
+	self.session.commit()
+	return lst
 
-class DBMgr():
-	# Database connection
-	DB_FILE = 'pom_db.sqlite'
-	DB_LINK = 'sqlite:///' + DB_FILE
-	DEBUG = True
+def GetSingleList(self, list_id):
+	""" Gets exact list (based on given list_id)
+	returns: single list
+	"""
+	print("Fetching single list from database")
+	singleList = self.session.query(ListModel).\
+		filter_by(identifier=list_id).first()
+	return singleList
 
-	def __init__(self):
-		# ORM base
-		self.Connect()
-		self.InitDB()		
-
-	def Connect(self):
-		""" Connect to database
-		"""
-		self.engine = create_engine(\
-			self.DB_LINK, echo=self.DEBUG)
-		session_factory = sessionmaker(bind=self.engine)
-		self.session = session_factory()
-
-	def InitDB(self):
-		""" Init database if it doesn't exists
-		"""
-		import ListModel
-		Base.metadata.create_all(self.engine)
-		self.session.commit()
-		lst = ListModel("name", 1)
-		self.session.add(lst)
-		self.session.commit()
-
-		#   Base.metadata.create_all(self.engine)
-		# # Create a bunch of records if we don't have any
-		# if self.session.query(ListModel).count() == 0:
-		# 	print("Creating new list in database")
-		# 	lst = ListModel(name, user_id)
-		# 	self.session.add(lst)
-
-
-	def ChangeList(self, list_id, name):
-		""" Perform changes in given list in database
-			list_id - id of the list
-			name - name of the list
-			user_id - id of the owner
-		returns: new List
-		"""
-		print("Modyfing list data in database")
-		lst = GetSingleList(list_id)
-		lst.Modify(name)
-		self.session.commit()
-		return lst
-
-	def GetSingleList(self, list_id):
-		""" Gets exact list (based on given list_id)
-		returns: single list
-		"""
-		print("Fetching single list from database")
-		singleList = self.session.query(ListModel).\
-			filter_by(identifier=list_id).first()
-		return singleList
-
-	def GetUserLists(self, user_id):
-		""" Gets every list that belongs to the user_id
-			user_id - id of user
-		returns: list of List objects
-		"""
-		print("Fetching lists from database")
-		lists = self.session.query(ListModel).\
-			filter_by(identifier=list_id).all()
-		return lists
+def GetUserLists(self, user_id):
+	""" Gets every list that belongs to the user_id
+		user_id - id of user
+	returns: list of List objects
+	"""
+	print("Fetching lists from database")
+	lists = self.session.query(ListModel).\
+		filter_by(identifier=list_id).all()
+	return lists
